223b8880f4fb4a5dfb31ccecf5a92bc20d388b3e
Ettore Leandro Tognoli <eleandro@redhat.com>
2021-05-12 15:25:19 +0200

    add quarkus

---
+++ quarkus-hello-world/src/main/java/org/acme/quarkus/hello/world/Main.java
@@ -0,0 +1,11 @@
+package org.acme.quarkus.hello.world;
+
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+   public static void main(String... args) {
+        System.out.println("Hello World!!!");
+   }
+}
